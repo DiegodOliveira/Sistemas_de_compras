36d2f9811ddcc175209ece6a9c39b1efe1ffd357
Diego de Oliveira Santana Cipriano <2022211510062@iesp.edu.br>
2023-08-21 21:29:02 -0300

    Atualização

--- src/diegotech/Aula02/NovaClasse.java
+++ src/diegotech/Aula02/NovaClasse.java
@@ -3,5 +3,7 @@ package diegotech.Aula02;
 public class NovaClasse {
     public static void main(String[] args) {
         System.out.println("Nova Classe");
+
+       NovaClasse novaClasse = new NovaClasse();
     }
 }
